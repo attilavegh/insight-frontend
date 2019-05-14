be4d8bc415230a85ba1c20245ee53a2c9d819035
Végh Attila <dev.attilavegh@gmail.com>
2019-05-15 01:28:27 +0200

    Add end-to-end test

--- apps/insight-frontend-e2e/src/app.e2e-spec.ts
+++ apps/insight-frontend-e2e/src/app.e2e-spec.ts
@@ -1,14 +1,25 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('Insight app', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to insight-frontend!');
+  it('should be able to login and send an insight', () => {
+    page.open();
+    page.login('elte.insight@gmail.com', 'insight_insightapp');
+
+    page.logout();
+    page.login('elte.insight.test1@gmail.com', 'ElteInsightTest1', true);
+
+    page.selectUser();
+    page.addContinueMessage();
+    page.sendInsight();
+
+    page.navigateToSentInsights();
+    expect(page.isSent).toBeTruthy();
   });
 });
--- apps/insight-frontend-e2e/src/app.po.ts
+++ apps/insight-frontend-e2e/src/app.po.ts
@@ -1,11 +1,79 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
+
+  private message = 'Keep up the good work';
+
+  open() {
     return browser.get('/');
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText();
+  login(email: string, password: string, hasExistingAccounts = false) {
+    const loginButton = element(by.css('.login__button'));
+    loginButton.click();
+
+    browser.driver.sleep(5000);
+
+    return browser.getAllWindowHandles().then((handles) => {
+      browser.switchTo().window(handles[1]).then(() => {
+        browser.ignoreSynchronization = true;
+
+        if (hasExistingAccounts) {
+          browser.driver.findElement(by.css('.eARute')).click();
+          browser.driver.sleep(3000);
+        }
+
+        browser.driver.findElement(by.id('identifierId')).sendKeys(email);
+        browser.driver.findElement(by.id('identifierNext')).click();
+
+        browser.driver.sleep(5000);
+
+        browser.driver.findElement(by.name('password')).sendKeys(password);
+        browser.driver.findElement(by.id('passwordNext')).click();
+
+        browser.driver.sleep(5000);
+        browser.ignoreSynchronization = false;
+      });
+    }).then(() => {
+      browser.getAllWindowHandles().then((handles) => {
+        browser.switchTo().window(handles[0]);
+      });
+    });
+  }
+
+  logout() {
+    browser.element(by.css('.insight-header__settings')).click();
+    browser.element(by.css('.insight-header__settings-menu-list-item')).click();
+  }
+
+  selectUser() {
+    const searchField = browser.element(by.css('.user-search__box-input'));
+    searchField.click();
+    searchField.sendKeys('Attila');
+
+    browser.sleep(3000);
+
+    browser.element.all(by.css('.user-search__dropdown-item')).get(0).click();
+  }
+
+  addContinueMessage() {
+    const inputBox = browser.element(by.css('.insight-input-box__text'));
+    inputBox.click();
+    inputBox.sendKeys(this.message);
+    browser.sleep(1000);
+  }
+
+  sendInsight() {
+    browser.element(by.css('.button-wrapper__button')).click();
+    browser.sleep(1000);
+  }
+
+  navigateToSentInsights() {
+    browser.get('/insights/sent');
+    browser.sleep(2000);
+  }
+
+  isSent() {
+    return !!browser.element(by.cssContainingText('.message__content-continue', this.message));
   }
 }
